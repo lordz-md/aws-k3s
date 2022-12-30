7853cb74c186244a661bc9afff568d13785f9d6d
lordz-md <Traian.Ciobanu@endava.com>
2022-12-30 23:20:28 +0200

    Update k3s-workers.sh

--- k3s-workers.sh
+++ k3s-workers.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-yum update && upgrade -y
+yum update -y && yum upgrade -y
 
 local_ip=$(curl -s http://169.254.169.254/latest/meta-data/local-ipv4)
-flannel_iface=$(ip -4 route ls | grep default | grep -Po '(?<=dev )(\S+)')
 provider_id="$(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone)/$(curl -s http://169.254.169.254/latest/meta-data/instance-id)"
 instance_id="$(curl -s http://169.254.169.254/latest/meta-data/instance-id)"
 
@@ -16,4 +15,4 @@ hostname $NEW_HOSTNAME
 sudo sed -i "s/$CUR_HOSTNAME/$NEW_HOSTNAME/g" /etc/hosts
 sudo sed -i "s/$CUR_HOSTNAME/$NEW_HOSTNAME/g" /etc/hostname
 
-curl -sfL https://get.k3s.io | INSTALL_K3S_VERSION=v1.25.5+k3s1 K3S_TOKEN=PuT1nHu1l0R4SAParasha K3S_URL=https://10.0.151.101:6443 sh -s - agent --node-ip $local_ip  --kubelet-arg="provider-id=aws:///$provider_id"
+curl -sfL https://get.k3s.io | INSTALL_K3S_VERSION=v1.25.5+k3s1 K3S_TOKEN=PuT1nHu1l0R4SAParasha K3S_URL=https://10.0.151.101:6443 sh -s - agent --node-ip $local_ip --flannel-backend=none --disable-cloud-controller --disable=servicelb --disable=traefik --kubelet-arg="provider-id=aws:///$provider_id"
