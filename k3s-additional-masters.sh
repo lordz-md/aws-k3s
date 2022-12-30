0bf229f4a1555a71bb0e1ceaab9f4d6cbe9ba9ea
lordz-md <Traian.Ciobanu@endava.com>
2022-12-30 22:28:24 +0200

    Create k3s-additional-masters.sh

---
+++ k3s-additional-masters.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+yum update -y && yum upgrade -y
+
+local_ip=$(curl -s http://169.254.169.254/latest/meta-data/local-ipv4)
+provider_id="$(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone)/$(curl -s http://169.254.169.254/latest/meta-data/instance-id)"
+instance_id="$(curl -s http://169.254.169.254/latest/meta-data/instance-id)"
+
+CUR_HOSTNAME=$(cat /etc/hostname)
+NEW_HOSTNAME=$instance_id
+
+hostnamectl set-hostname $NEW_HOSTNAME
+hostname $NEW_HOSTNAME
+
+sudo sed -i "s/$CUR_HOSTNAME/$NEW_HOSTNAME/g" /etc/hosts
+sudo sed -i "s/$CUR_HOSTNAME/$NEW_HOSTNAME/g" /etc/hostname
+
+curl -sfL https://get.k3s.io | INSTALL_K3S_VERSION=v1.25.5+k3s1 K3S_TOKEN=PuT1nHu1l0R4SAParasha sh -s - server --server https://10.0.151.101:6443 --node-ip $local_ip --advertise-address $local_ip  --kubelet-arg="cloud-provider=external" --flannel-backend=none  --disable-cloud-controller --disable=servicelb --disable=traefik --write-kubeconfig-mode 644 --kubelet-arg="provider-id=aws:///$provider_id"
