c089712270977519d32bf4473fb0838a62e724e5
lordz-md <Traian.Ciobanu@endava.com>
2023-01-01 13:43:01 +0200

    add variable for initial master ip

--- k3s-additional-masters.sh
+++ k3s-additional-masters.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+master_ip=
 yum update -y && yum upgrade -y
 
 local_ip=$(curl -s http://169.254.169.254/latest/meta-data/local-ipv4)
@@ -15,4 +16,4 @@ hostname $NEW_HOSTNAME
 sudo sed -i "s/$CUR_HOSTNAME/$NEW_HOSTNAME/g" /etc/hosts
 sudo sed -i "s/$CUR_HOSTNAME/$NEW_HOSTNAME/g" /etc/hostname
 
-curl -sfL https://get.k3s.io | INSTALL_K3S_VERSION=v1.25.5+k3s1 K3S_TOKEN=PuT1nHu1l0R4SAParasha sh -s - server --server https://10.0.151.101:6443 --node-ip $local_ip --advertise-address $local_ip  --kubelet-arg="cloud-provider=external" --flannel-backend=none --disable-network-policy --cluster-cidr=192.168.0.0/16 --disable-cloud-controller --disable servicelb --disable traefik --kubelet-arg="provider-id=aws:///$provider_id"
+curl -sfL https://get.k3s.io | INSTALL_K3S_VERSION=v1.25.5+k3s1 K3S_TOKEN=PuT1nHu1l0R4SAParasha sh -s - server --server "https://$master_ip:6443" --node-ip $local_ip --advertise-address $local_ip  --kubelet-arg="cloud-provider=external" --flannel-backend=none --disable-network-policy --cluster-cidr=192.168.0.0/16 --disable-cloud-controller --disable servicelb --disable traefik --kubelet-arg="provider-id=aws:///$provider_id"
