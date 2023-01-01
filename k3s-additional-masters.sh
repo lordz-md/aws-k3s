35a6510b0302d882aff2e2350b5e976d97d7ead6
lordz-md <Traian.Ciobanu@endava.com>
2023-01-01 18:13:29 +0200

    use k3s version 1.24

--- k3s-additional-masters.sh
+++ k3s-additional-masters.sh
@@ -18,4 +18,4 @@ hostname $NEW_HOSTNAME
 sudo sed -i "s/$CUR_HOSTNAME/$NEW_HOSTNAME/g" /etc/hosts
 sudo sed -i "s/$CUR_HOSTNAME/$NEW_HOSTNAME/g" /etc/hostname
 
-curl -sfL https://get.k3s.io | INSTALL_K3S_VERSION=v1.25.5+k3s1 K3S_TOKEN=PuT1nHu1l0R4SAParasha sh -s - server --server "https://$master_ip:6443" --node-ip $local_ip --advertise-address $local_ip  --kubelet-arg="cloud-provider=external" --flannel-backend=none --disable-network-policy --cluster-cidr=192.168.0.0/16 --disable-cloud-controller --disable servicelb --disable traefik --kubelet-arg="provider-id=aws:///$provider_id"
+curl -sfL https://get.k3s.io | INSTALL_K3S_VERSION=v1.24.8+k3s1 K3S_TOKEN=PuT1nHu1l0R4SAParasha sh -s - server --server "https://$master_ip:6443" --node-ip $local_ip --advertise-address $local_ip  --kubelet-arg="cloud-provider=external" --flannel-backend=none --disable-network-policy --cluster-cidr=192.168.0.0/16 --disable-cloud-controller --disable servicelb --disable traefik --kubelet-arg="provider-id=aws:///$provider_id"
