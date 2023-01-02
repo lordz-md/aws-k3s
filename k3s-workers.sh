19acbbbe4b4e9bd309ad07c984e3dc9d728b907d
lordz-md <Traian.Ciobanu@endava.com>
2023-01-02 11:51:42 +0200

    add git-core and some environment vars

--- k3s-workers.sh
+++ k3s-workers.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 yum update -y && yum upgrade -y
-yum install jq -y 
+yum install git-core jq -y 
 
 aws_region=$(curl -s http://169.254.169.254/latest/dynamic/instance-identity/document | jq .region -r)
 master_ip=$(aws ec2 describe-instances --filters "Name=tag:InitialNode,Values=1" --query 'Reservations[*].Instances[*].PrivateIpAddress' --output text --region "$aws_region")
@@ -20,3 +20,10 @@ sudo sed -i "s/$CUR_HOSTNAME/$NEW_HOSTNAME/g" /etc/hosts
 sudo sed -i "s/$CUR_HOSTNAME/$NEW_HOSTNAME/g" /etc/hostname
 
 curl -sfL https://get.k3s.io | INSTALL_K3S_VERSION=v1.24.9+k3s1 K3S_TOKEN=PuT1nHu1l0R4SAParasha K3S_URL=https://"$master_ip":6443 sh -s - agent --node-ip $local_ip --kubelet-arg="provider-id=aws:///$provider_id"
+
+sleep 5
+echo "source <(kubectl completion bash)" >> ~/.bashrc
+echo 'alias k=kubectl' >> ~/.bashrc
+echo 'complete -o default -F __start_kubectl k' >> ~/.bashrc
+sleep 5
+source ~/.bashrc
