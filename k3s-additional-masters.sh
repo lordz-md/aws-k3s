cd22c841da37fa128a319a374cae338b22c1fdff
lordz-md <Traian.Ciobanu@endava.com>
2023-01-02 11:42:17 +0200

    add  sleep, alias for kubernetes and autocomplete

--- k3s-additional-masters.sh
+++ k3s-additional-masters.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 yum update -y && yum upgrade -y
-yum install jq -y 
+yum install jq -y
+sleep 15
 
 aws_region=$(curl -s http://169.254.169.254/latest/dynamic/instance-identity/document | jq .region -r)
 master_ip=$(aws ec2 describe-instances --filters "Name=tag:InitialNode,Values=1" --query 'Reservations[*].Instances[*].PrivateIpAddress' --output text --region "$aws_region")
@@ -19,3 +20,10 @@ sudo sed -i "s/$CUR_HOSTNAME/$NEW_HOSTNAME/g" /etc/hosts
 sudo sed -i "s/$CUR_HOSTNAME/$NEW_HOSTNAME/g" /etc/hostname
 
 curl -sfL https://get.k3s.io | INSTALL_K3S_VERSION=v1.24.9+k3s1 K3S_TOKEN=PuT1nHu1l0R4SAParasha sh -s - server --server https://$master_ip:6443 --node-ip $local_ip --advertise-address $local_ip  --kubelet-arg="cloud-provider=external" --flannel-backend=none --disable-network-policy --cluster-cidr=192.168.0.0/16 --disable-cloud-controller --disable servicelb --disable traefik --kubelet-arg="provider-id=aws:///$provider_id"
+
+sleep 5
+echo "source <(kubectl completion bash)" >> ~/.bashrc
+echo 'alias k=kubectl' >> ~/.bashrc
+echo 'complete -o default -F __start_kubectl k' >> ~/.bashrc
+sleep 5
+source ~/.bashrc
